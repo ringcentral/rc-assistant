1714fc34e135cf8f23fedaa28bef20016835a5e1
Tyler Long <tyler4long@gmail.com>
2018-11-27 19:28:00 +0800

    Add a test case about AWS Lex

---
+++ test/lex.spec.js
@@ -0,0 +1,25 @@
+/* eslint-env jest */
+import Lexruntime from 'aws-sdk/clients/lexruntime'
+
+jest.setTimeout(16000)
+
+const lexruntime = new Lexruntime({ region: 'us-east-1' })
+
+describe('AWS Lex', () => {
+  test('default', async () => {
+    const data = await lexruntime.postText({
+      botAlias: 'GlipBot',
+      botName: 'GlipBot',
+      inputText: 'presence info',
+      userId: '123456'
+    }).promise()
+    expect(data).toEqual({
+      intentName: 'PresenceInfo',
+      slots: {},
+      message: null,
+      messageFormat: null,
+      dialogState: 'ReadyForFulfillment',
+      slotToElicit: null
+    })
+  })
+})
