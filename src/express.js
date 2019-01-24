01eeb70deb48a0d1db806dc1bbcd01117e283d19
Tyler Long <tyler4long@gmail.com>
2019-01-24 11:13:59 +0800

    Add callback for app.listen

--- src/express.js
+++ src/express.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 import app from './app'
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000')
+})
 
 setInterval(() => axios.put(`${process.env.RINGCENTRAL_CHATBOT_SERVER}/admin/maintain`), 86400000)
