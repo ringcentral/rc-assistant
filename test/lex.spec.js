64c1fe12ad6fdf46a85d7c0ddf23b09676631991
Tyler Long <tyler4long@gmail.com>
2019-04-03 13:32:33 +0800

    Refactor Lex intent tests

--- test/lex.spec.js
+++ test/lex.spec.js
@@ -5,50 +5,31 @@ jest.setTimeout(16000)
 
 const lexruntime = new Lexruntime({ region: 'us-east-1' })
 
+const getIntent = async inputText => {
+  const data = await lexruntime.postText({
+    botAlias: 'GlipBot',
+    botName: 'GlipBot',
+    inputText,
+    userId: '123456',
+    sessionAttributes: {
+      hello: '111'
+    }
+  }).promise()
+  console.log(data)
+  return data.intentName
+}
+
 describe('AWS Lex', () => {
   test('presence info', async () => {
-    const data = await lexruntime.postText({
-      botAlias: 'GlipBot',
-      botName: 'GlipBot',
-      inputText: 'presence info',
-      userId: '123456',
-      sessionAttributes: {
-        hello: '111'
-      }
-    }).promise()
-    expect(data).toEqual({
-      intentName: 'PresenceInfo',
-      slots: {},
-      message: null,
-      messageFormat: null,
-      dialogState: 'ReadyForFulfillment',
-      slotToElicit: null,
-      sessionAttributes: {
-        hello: '111'
-      }
-    })
+    const intentName = await getIntent('presence info')
+    expect(intentName).toEqual('PresenceInfo')
   })
-
   test('company information', async () => {
-    const data = await lexruntime.postText({
-      botAlias: 'GlipBot',
-      botName: 'GlipBot',
-      inputText: 'company information',
-      userId: '123456',
-      sessionAttributes: {
-        hello: '111'
-      }
-    }).promise()
-    expect(data).toEqual({
-      intentName: 'CompanyInfo',
-      slots: {},
-      message: null,
-      messageFormat: null,
-      dialogState: 'ReadyForFulfillment',
-      slotToElicit: null,
-      sessionAttributes: {
-        hello: '111'
-      }
-    })
+    const intentName = await getIntent('company information')
+    expect(intentName).toEqual('CompanyInfo')
+  })
+  test('company info', async () => {
+    const intentName = await getIntent('company info')
+    expect(intentName).toEqual('CompanyInfo')
   })
 })
