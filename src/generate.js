6504222f80bf6e56763a5f419f01d28dfe7e19aa
Tyler Long <tyler4long@gmail.com>
2019-04-11 10:14:00 +0800

    Refactor code

--- src/generate.js
+++ src/generate.js
@@ -31,3 +31,40 @@ export const generateSlotUtterances = (action, subject, slot) => {
     R.slice(0, 10)
   )(utterances)
 }
+
+export const generateLex = (intents, slotTypes) => {
+  return {
+    'metadata': {
+      'schemaVersion': '1.0',
+      'importType': 'LEX',
+      'importFormat': 'JSON'
+    },
+    'resource': {
+      'name': 'RCAssistant',
+      'version': '1',
+      intents,
+      slotTypes,
+      'voiceId': '0',
+      'childDirected': false,
+      'locale': 'en-US',
+      'idleSessionTTLInSeconds': 600,
+      'clarificationPrompt': {
+        'messages': [
+          {
+            'contentType': 'PlainText',
+            'content': 'Sorry, I did not understand, can you rephrase that?'
+          }
+        ],
+        'maxAttempts': 5
+      },
+      'abortStatement': {
+        'messages': [
+          {
+            'contentType': 'PlainText',
+            'content': 'Sorry, I could not understand. Please try again.'
+          }
+        ]
+      }
+    }
+  }
+}
--- test/generate.spec.js
+++ test/generate.spec.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 import fs from 'fs'
 
-import { generateIntentUtterances, generateSlotUtterances } from '../src/generate'
+import { generateIntentUtterances, generateSlotUtterances, generateLex } from '../src/generate'
 
 describe('generateIntentUtterances', () => {
   test('view business hour', () => {
@@ -96,40 +96,7 @@ describe('generate whole file', () => {
         'valueSelectionStrategy': 'TOP_RESOLUTION'
       }
     ]
-    const lex = {
-      'metadata': {
-        'schemaVersion': '1.0',
-        'importType': 'LEX',
-        'importFormat': 'JSON'
-      },
-      'resource': {
-        'name': 'RCAssistant',
-        'version': '1',
-        intents,
-        slotTypes,
-        'voiceId': '0',
-        'childDirected': false,
-        'locale': 'en-US',
-        'idleSessionTTLInSeconds': 600,
-        'clarificationPrompt': {
-          'messages': [
-            {
-              'contentType': 'PlainText',
-              'content': 'Sorry, I did not understand, can you rephrase that?'
-            }
-          ],
-          'maxAttempts': 5
-        },
-        'abortStatement': {
-          'messages': [
-            {
-              'contentType': 'PlainText',
-              'content': 'Sorry, I could not understand. Please try again.'
-            }
-          ]
-        }
-      }
-    }
+    const lex = generateLex(intents, slotTypes)
     fs.writeFileSync('aws_lex_generated.json', JSON.stringify(lex, null, 2))
   })
 })
